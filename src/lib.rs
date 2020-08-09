aeaf14c595b86f3215e5f412778129e3e0f8f39e
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-08-09 20:16:10 +0300

    Returns collections of capabilities.
    This is an experiment comparing ease of use of collections vs iter_xxx
    functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use unibilium_sys::{unibi_boolean, unibi_from_env, unibi_from_term, unibi_numeric, unibi_term};
+use std::ffi::CStr;
+use unibilium_sys::{
+    unibi_boolean, unibi_from_env, unibi_from_term, unibi_numeric, unibi_string, unibi_term,
+};
 
 pub struct UnibiTerm {
     term: *mut unibi_term,
@@ -25,6 +28,69 @@ impl UnibiTerm {
         UnibiTerm {
             term: unsafe { unibi_from_term(name.as_ptr() as *const i8) },
         }
+    }
+
+    pub fn booleans(&self) -> Vec<(unibi_boolean, bool)> {
+        let mut all = vec![];
+        let first = unibi_boolean::unibi_boolean_begin_.0 + 1;
+        let end = unibi_boolean::unibi_boolean_end_.0;
+        for current in first..end {
+            let b = unibi_boolean(current);
+            let value = unsafe { unibilium_sys::unibi_get_bool(self.term, b) };
+            let value = value > 0;
+            let value = (b, value);
+            all.push(value);
+        }
+        all
+    }
+
+    pub fn ext_booleans(&self) -> Vec<(Option<String>, bool)> {
+        let mut all = vec![];
+        let end = unsafe { unibilium_sys::unibi_count_ext_bool(self.term) };
+        for current in 0..end {
+            let key = unsafe { unibilium_sys::unibi_get_ext_bool_name(self.term, current) };
+            let key = self.c_char_to_string(key);
+            let value = unsafe { unibilium_sys::unibi_get_ext_bool(self.term, current) };
+            let value = value > 0;
+            let value = (key, value);
+            all.push(value);
+        }
+        all
+    }
+
+    pub fn numerics(&self) -> Vec<(unibi_numeric, i32)> {
+        let mut all = vec![];
+        let first = unibi_numeric::unibi_numeric_begin_.0 + 1;
+        let end = unibi_numeric::unibi_numeric_end_.0;
+        for current in first..end {
+            let b = unibi_numeric(current);
+            let value = unsafe { unibilium_sys::unibi_get_num(self.term, b) };
+            let value = (b, value);
+            all.push(value);
+        }
+        all
+    }
+
+    fn c_char_to_string(&self, ptr: *const i8) -> Option<String> {
+        if ptr.is_null() {
+            None
+        } else {
+            let value = unsafe { CStr::from_ptr(ptr) };
+            let value = value.to_string_lossy().into_owned();
+            Some(value)
+        }
+    }
+
+    pub fn strings(&self) -> Vec<(unibi_string, Option<String>)> {
+        let mut all = vec![];
+        let first = unibi_string::unibi_string_begin_.0 + 1;
+        let end = unibi_string::unibi_string_end_.0;
+        for current in first..end {
+            let s = unibi_string(current);
+            let value = unsafe { unibilium_sys::unibi_get_str(self.term, s) };
+            all.push((s, self.c_char_to_string(value)));
+        }
+        all
     }
 
     pub fn iter_bool(&self) -> UnibiTermBoolIter {
