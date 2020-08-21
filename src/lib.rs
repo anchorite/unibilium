e594014e5ac3e8c855a7ca1a15b94fa945f98f4f
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-08-22 01:24:37 +0300

    Adds ExtString struct for extended string caps.

--- src/lib.rs
+++ src/lib.rs
@@ -170,22 +170,60 @@ impl<'a> String<'a> {
     /// done according to the rules by std::ascii::escape_default, with the exception that
     /// escape(0x1b) is represented as '^['.
     pub fn escaped_value(&self) -> Option<std::string::String> {
-        self.value().map(|value| {
-            std::string::String::from_utf8(
-                value
-                    .as_bytes()
-                    .iter()
-                    .map(|c| std::ascii::escape_default(*c))
-                    .flatten()
-                    .collect(),
-            )
-            .expect("Invalid UTF-8 string encountered")
-            .replace("\\x1b", "^[")
-        })
+        escape_string(self.value())
     }
 }
 
 impl<'a> fmt::Display for String<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.escaped_value() {
+            None => write!(f, "{}: NULL", self.name()),
+            Some(value) => write!(f, "{}: {}", self.name(), value),
+        }
+    }
+}
+
+pub struct ExtString<'a> {
+    index: u64,
+    term: &'a Term,
+}
+
+impl<'a> ExtString<'a> {
+    fn from_index(index: u64, term: &'a Term) -> Self {
+        ExtString { index, term }
+    }
+
+    pub fn name(&self) -> &str {
+        // Returns static string if called with value between 0 and count
+        let name = unsafe { unibilium_sys::unibi_get_ext_str_name(self.term.term, self.index) };
+        if name.is_null() {
+            panic!(
+                "Invalid index for extended string capability: {}",
+                self.index
+            );
+        }
+        let name = unsafe { CStr::from_ptr(name) };
+        name.to_str().expect("Invalid UTF-8 string encountered")
+    }
+
+    pub fn value(&self) -> Option<&str> {
+        let value = unsafe { unibilium_sys::unibi_get_ext_str(self.term.term, self.index) };
+        if value.is_null() {
+            return None;
+        }
+        let value = unsafe { CStr::from_ptr(value) };
+        Some(value.to_str().expect("Invalid UTF-8 string encountered"))
+    }
+
+    /// Returns escaped std::string::String representing the value of the capability. Escaping is
+    /// done according to the rules by std::ascii::escape_default, with the exception that
+    /// escape(0x1b) is represented as '^['.
+    pub fn escaped_value(&self) -> Option<std::string::String> {
+        escape_string(self.value())
+    }
+}
+
+impl<'a> fmt::Display for ExtString<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.escaped_value() {
             None => write!(f, "{}: NULL", self.name()),
@@ -259,6 +297,16 @@ impl Term {
         }
         all
     }
+
+    pub fn ext_strings(&self) -> Vec<ExtString> {
+        let mut all = vec![];
+        let end = unsafe { unibilium_sys::unibi_count_ext_str(self.term) };
+        for index in 0..end {
+            let b = ExtString::from_index(index, self);
+            all.push(b);
+        }
+        all
+    }
 }
 
 impl Drop for Term {
@@ -267,4 +315,18 @@ impl Drop for Term {
             unibilium_sys::unibi_destroy(self.term);
         }
     }
+}
+
+fn escape_string(s: Option<&str>) -> Option<std::string::String> {
+    s.map(|s| {
+        std::string::String::from_utf8(
+            s.as_bytes()
+                .iter()
+                .map(|c| std::ascii::escape_default(*c))
+                .flatten()
+                .collect(),
+        )
+        .expect("Invalid UTF-8 string encountered")
+        .replace("\\x1b", "^[")
+    })
 }
