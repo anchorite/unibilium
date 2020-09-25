7588fa63e45486f27d609b075a83c021c4397e2c
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-09-26 01:55:49 +0300

    Provides main library documentation and examples.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,75 @@
+//! This library provides safe read-only access to unibilium C library.
+//!
+//! The library provides means to create a `Term` struct representing a terminal. Using this struct
+//! you can read boolean, numeric and string capabilities of the terminal. You can also read the
+//! extended versions of each of the above capabilities.
+//!
+//! Currently you cannot modify capabilities or add new extended ones.
+//!
+//! # Examples
+//!
+//! Create a Term struct using TERM environment variable.
+//!
+//! ```
+//! # use std::error::Error;
+//! #
+//! # fn main() -> Result<(), Box<dyn Error>> {
+//! use unibilium::Term;
+//!
+//! let term = Term::from_env()?;
+//! #
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! Create a Term struct for specific terminal.
+//!
+//! ```
+//! # use std::error::Error;
+//! #
+//! # fn main() -> Result<(), Box<dyn Error>> {
+//! use unibilium::Term;
+//!
+//! let term = Term::from_term_name("vt100")?;
+//! #
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! Dump boolean capabilities of a terminal.
+//!
+//! ```
+//! # use std::error::Error;
+//! #
+//! # fn main() -> Result<(), Box<dyn Error>> {
+//! use unibilium::Term;
+//!
+//! let term = Term::from_term_name("vt100")?;
+//! for bool_cap in term.booleans() {
+//!     println!("{}", bool_cap);
+//! }
+//! #
+//! #     Ok(())
+//! # }
+//! ```
+//!
+//! Dump extended boolean capabilities of a terminal.
+//!
+//! ```
+//! # use std::error::Error;
+//! #
+//! # fn main() -> Result<(), Box<dyn Error>> {
+//! use unibilium::Term;
+//!
+//! let term = Term::from_term_name("vt100")?;
+//! for bool_cap in term.ext_booleans() {
+//!     println!("{}", bool_cap);
+//! }
+//! #
+//! #     Ok(())
+//! # }
+//! ```
+
 pub mod boolean;
 pub mod error;
 pub mod numeric;
