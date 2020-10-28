a2d6a86e0c2ef1395a60ebd6adf068590c629462
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-10-29 00:12:26 +0200

    Rewords documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-//! This library provides safe read-only access to unibilium C library.
+//! This library provides safe read-only access to unibilium C library. The latter gives read/write
+//! access to terminal capabilities from termcap database.
 //!
-//! The library provides means to create a `Term` struct representing a terminal. Using this struct
-//! you can read boolean, numeric and string capabilities of the terminal. You can also read the
-//! extended versions of each of the above capabilities.
+//! This library provides means to create a `Term` struct representing a terminal. Using this
+//! struct you can read boolean, numeric and string capabilities of the terminal. You can also read
+//! the extended versions of each of the above capabilities.
 //!
 //! Currently you cannot modify capabilities or add new extended ones.
 //!
